1dd6b7d0a9c87c69a5acc596a450d40a7f34c13c
danyprastya <danyprastyaalhakim@gmail.com>
2025-04-20 14:54:22 +0700

    test CI/CD 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const AWS = require('aws-sdk');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 require('dotenv').config();
 
@@ -27,25 +27,28 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  const fileContent = fs.readFileSync(req.file.path);
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(req.file.path);
 
-  const params = {
-    Bucket: process.env.S3_BUCKET_NAME,
-    Key: req.file.originalname,
-    Body: fileContent,
-  };
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: req.file.originalname,
+      Body: fileContent,
+    };
 
-  s3.upload(params, (err, data) => {
-    fs.unlinkSync(req.file.path); // hapus file lokal setelah upload
-    if (err) {
-      return res.send("Upload gagal: " + err);
-    }
+    const data = await s3.upload(params).promise();
+    
+    // Hapus file lokal setelah upload
+    await fs.unlink(req.file.path);
+    
     res.redirect('/');
-  });
+  } catch (err) {
+    res.send("Upload gagal: " + err);
+  }
 });
 
 app.listen(3000, '0.0.0.0', () => {
   console.log('Server running at http://localhost:3000');
   console.log("🚀 Update baru di " + new Date());
-});// test deploy
+});
